e84a5ddce9f9203ef3534330d4eccb60871f42b7
zerock <kangckck24@gmail.com>
2025-04-08 10:13:10 +0900

    ddd

--- src/main/java/org/zerock/sb7/board/domain/Board.java
+++ src/main/java/org/zerock/sb7/board/domain/Board.java
@@ -5,8 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Board {
 
     @Id

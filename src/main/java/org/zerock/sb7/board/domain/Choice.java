2945f80cc906670adc275ebf4822b2c5b2e7aa20
zerock <kangckck24@gmail.com>
2025-04-08 10:17:54 +0900

    ddd

---
+++ src/main/java/org/zerock/sb7/board/domain/Choice.java
@@ -0,0 +1,7 @@
+package org.zerock.sb7.board.domain;
+
+public enum Choice {
+
+    LIKE, DISLIKE;
+
+}
---
+++ src/main/java/org/zerock/sb7/board/domain/Favorite.java
@@ -0,0 +1,34 @@
+package org.zerock.sb7.board.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer fid;
+
+    private String mid;
+
+    private Board board;
+
+    private Choice choice;
+
+}
+
+
+
+
+
+
+

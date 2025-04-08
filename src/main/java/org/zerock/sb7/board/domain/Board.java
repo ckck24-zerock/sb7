debc3018e3c96b557154b17211644215b29117e3
zerock <kangckck24@gmail.com>
2025-04-08 10:11:24 +0900

    ddd

---
+++ src/main/java/org/zerock/sb7/board/domain/Board.java
@@ -0,0 +1,16 @@
+package org.zerock.sb7.board.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bno;
+
+}

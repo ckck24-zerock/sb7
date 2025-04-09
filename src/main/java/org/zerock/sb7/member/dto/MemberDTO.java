583db96e4726089e627ff54f668928d799d67ad0
zerock <kangckck24@gmail.com>
2025-04-09 13:31:30 +0900

    ddd

---
+++ src/main/java/org/zerock/sb7/member/dto/MemberDTO.java
@@ -0,0 +1,25 @@
+package org.zerock.sb7.member.dto;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MemberDTO implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
--- src/test/java/org/zerock/sb7/member/MemberRepoTest.java
+++ src/test/java/org/zerock/sb7/member/MemberRepoTest.java
@@ -42,4 +42,11 @@ public class MemberRepoTest {
         }//end for
     }
 
+    @Test
+    public void testSelectOne() {
+        String mid = "user19";
+
+        log.info(memberRepo.selectOne(mid));
+    }
+
 }

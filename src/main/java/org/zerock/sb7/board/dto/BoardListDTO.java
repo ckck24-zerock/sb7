da18b4de11651b6bc8471f4b769a8e9614cdd730
zerock <kangckck24@gmail.com>
2025-04-09 10:18:56 +0900

    ddd

---
+++ src/main/java/org/zerock/sb7/board/dto/BoardListDTO.java
@@ -0,0 +1,15 @@
+package org.zerock.sb7.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardListDTO {
+
+    private Integer bno;
+    private String title;
+    private String writer;
+    private String fileName;
+    private long favoriteCount;
+    private long replyCount;
+
+}

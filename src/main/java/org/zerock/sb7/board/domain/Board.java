a459cfbeaa07e31d02c441d1c79f274dc45ffaf8
zerock <kangckck24@gmail.com>
2025-04-09 10:30:54 +0900

    ddd

--- src/main/java/org/zerock/sb7/board/domain/Board.java
+++ src/main/java/org/zerock/sb7/board/domain/Board.java
@@ -28,10 +28,13 @@ public class Board {
 
     @ElementCollection(fetch = FetchType.LAZY)
     @BatchSize(size = 100)
+    @Builder.Default
     private List<BoardImage> images = new ArrayList<>();
 
     @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "board_tags", joinColumns = @JoinColumn(name = "board_bno"))
     @BatchSize(size = 100)
+    @Builder.Default
     private List<String> tags = new ArrayList<>();
 
 

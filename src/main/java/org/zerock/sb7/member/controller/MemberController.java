c1194bfb6a356f47a9ea4355d0b7cb76927a6f69
zerock <kangckck24@gmail.com>
2025-04-10 13:15:55 +0900

    ddd

--- src/main/java/org/zerock/sb7/member/controller/MemberController.java
+++ src/main/java/org/zerock/sb7/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/member")
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     @GetMapping("login")
-    public void login() {
+    public void login( @RequestParam(required = false, name = "error") String error) {
         log.info("GET login");
+        log.info("error:" + error);
+        log.info(error == null ?"null": "exist");
     }
 }

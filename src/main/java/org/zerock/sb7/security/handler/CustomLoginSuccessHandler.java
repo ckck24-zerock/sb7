a4b7345ee153f3920fc4529121053fced37ab61d
zerock <kangckck24@gmail.com>
2025-04-10 10:46:10 +0900

    ddd

--- src/main/java/org/zerock/sb7/security/handler/CustomLoginSuccessHandler.java
+++ src/main/java/org/zerock/sb7/security/handler/CustomLoginSuccessHandler.java
@@ -4,19 +4,33 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
 
+@Log4j2
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authentication) throws IOException, ServletException {
+
+        log.info("-----success handler 4 -----");
+
         AuthenticationSuccessHandler.super.onAuthenticationSuccess(request, response, chain, authentication);
     }
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+
+        log.info("-----success handler 3 -----");
 
     }
 }

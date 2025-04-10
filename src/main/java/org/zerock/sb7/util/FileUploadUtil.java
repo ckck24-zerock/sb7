59ec9cc2962c23ebcef4e2160564066145514a44
zerock <kangckck24@gmail.com>
2025-04-10 14:11:06 +0900

    ddd

---
+++ src/main/java/org/zerock/sb7/util/FileUploadUtil.java
@@ -0,0 +1,42 @@
+package org.zerock.sb7.util;
+
+import jakarta.annotation.PostConstruct;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class FileUploadUtil {
+
+    @Value("${org.zerock.upload}")
+    private String uploadDir;
+
+    private final ResourceLoader resourceLoader;
+
+    @PostConstruct
+    public void ready()throws Exception{
+        log.info("---------------post construct---------------");
+        log.info("uploadDir: " + uploadDir);
+
+        Resource resource = resourceLoader.getResource("classpath:/upload/somefile.txt");
+        File uploadDirFile = new File(uploadDir);
+
+        log.info("uploadDirFile: " + uploadDirFile.getAbsolutePath());
+
+        if(!uploadDirFile.exists()){
+            uploadDirFile.mkdirs();
+        }
+
+
+    }
+
+
+}

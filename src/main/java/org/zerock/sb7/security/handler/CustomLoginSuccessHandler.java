f771ce728d8fcf7e93f572755cf3e4bbe5dcde6c
zerock <kangckck24@gmail.com>
2025-04-10 10:59:19 +0900

    ddd

--- src/main/java/org/zerock/sb7/security/handler/CustomLoginSuccessHandler.java
+++ src/main/java/org/zerock/sb7/security/handler/CustomLoginSuccessHandler.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.zerock.sb7.member.dto.MemberDTO;
 
 import java.io.IOException;
 
@@ -31,6 +32,17 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
             Authentication authentication) throws IOException, ServletException {
 
         log.info("-----success handler 3 -----");
+        log.info(authentication);
+
+        MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+
+        log.info(memberDTO);
+
+        //만일 social 회원이라면 회원정보를 수정하는 페이지로 이동
+
+        //아니면 '/'이동
+        response.sendRedirect("/");
+
 
     }
 }

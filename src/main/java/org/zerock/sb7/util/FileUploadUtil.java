449f7ebcb4422c09812b9a05ea35a0d49817ea82
zerock <kangckck24@gmail.com>
2025-04-10 14:29:35 +0900

    ddd

--- src/main/java/org/zerock/sb7/util/FileUploadUtil.java
+++ src/main/java/org/zerock/sb7/util/FileUploadUtil.java
@@ -8,11 +8,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 @Log4j2
@@ -38,6 +42,21 @@ public class FileUploadUtil {
     public List<String> uploadFiles (List<MultipartFile> files) throws Exception{
 
         List<String> uploadedFileNames = new ArrayList<>();
+
+        if(files != null && !files.isEmpty()){
+
+            for(MultipartFile file : files){
+
+                String saveFileName = UUID.randomUUID().toString()+"_" + file.getOriginalFilename();
+
+                Path path = Paths.get(uploadDir, saveFileName);
+
+                FileCopyUtils.copy(file.getBytes(), path.toFile());
+
+            }//end for
+
+        }//end if
+
 
         return uploadedFileNames;
     }
